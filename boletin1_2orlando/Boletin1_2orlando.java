cbbf14906cc46a5915de1a9e47a702d0807c7396
Orlando Joel Yagual Endara <oyagualendara@nautilus31.ad.danielcastelao.org>
2019-10-08 11:18:12 +0200

    first commit

---
+++ boletin1_2orlando/Boletin1_2orlando.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_2orlando;
+
+/**
+ *
+ * @author oyagualendara
+ */
+public class Boletin1_2orlando {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int lado=3,a;
+        a=lado*lado;
+System.out.println("El area de un cuadrado es lado "+lado+" por lado " +lado+" el resultado = "+a);
+    }
+    
+}
